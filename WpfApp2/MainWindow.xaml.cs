65f3cf56dd2bea9e4fec7658424a9dc03baa905d
Miguel <migfrydenlund@gmail.com>
2025-04-30 14:51:34 +0200

    Several changes made.

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,46 +24,73 @@ namespace WpfApp2
         private MMDeviceEnumerator deviceEnumerator; // This will enumerate audio devices
         private MMDevice defaultDevice; // This represents the default audio output device
 
+        private bool isManualChange = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            debug();
+    
 
             // Initialize NAudio
             deviceEnumerator = new MMDeviceEnumerator();  // Create a new instance to manage audio devices
             defaultDevice = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
             // Get the default audio output device (like speakers or headphones)
 
-            // Check if defaultDevice is null
-            if (defaultDevice == null)
-            {
-                VolumeTextBlock.Text = "No default audio device found. Please check your audio settings.";
-                return; // Exit if no audio device is available
-            }
-            else
-            {
-                // If a device is found, display information about it
-                VolumeTextBlock.Text = $"Default Audio Device: {defaultDevice.DeviceFriendlyName}";
-                VolumeTextBlock.Text += $"State: {defaultDevice.State}";
-                VolumeTextBlock.Text += $"Volume Level: {defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar * 100}%";
-            }
-
-            VolumeTextBlock.Text = $"Volume: {VolumeSlider.Value}"; // This can proceed if defaultDevice is valid
 
         }
 
+        
+
+        public void debug()
+        {
+            if (VolumeTextBlock.IsLoaded)
+            {
+                VolumeTextBlock.Text = "It's loaded!";
+            }
+            else
+            {
+                VolumeTextBlock.Loaded += (s, e) => { VolumeTextBlock.Text = "Now it's loaded!"; };
+            }
+        }
+
+        
+
+        public void MouseOverMove(object sender, MouseEventArgs e)
+        {
+            
+                
+                float randomVolume = random.Next(0, 101);
+                VolumeSlider.Value = randomVolume; // Set to random value
+                
+            
+        }
+
+
         public void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            if (e.NewValue != e.OldValue)
+
+            if (VolumeTextBlock.IsLoaded)
             {
-                float randomVolume = random.Next(0, 101);
-                VolumeSlider.Value = randomVolume;
-                SetSystemVolume(randomVolume / 100);
-                VolumeTextBlock.Text = $"Volume: {randomVolume.ToString()}";
+                SetSystemVolume((float)e.NewValue / 100);
+                VolumeTextBlock.Text = GetRandomSpan((float)e.NewValue);
             }
+        }
+
+        public string GetRandomSpan(float randomVolume)
+        {
+            float minValue = randomVolume - random.Next(0, 20);
+            float maxValue = randomVolume + random.Next(0, 20);
+
+            return $"Your volume is between {minValue} and {maxValue}";
         }
 
         private void SetSystemVolume(float volume)
         {
+            if (defaultDevice == null)
+            {
+                return;
+            }
             // Sets the volume to the system's master volume level
             defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar = volume;
         }
